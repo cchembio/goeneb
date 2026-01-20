02d4e99b1059e6c15fb36080fa5324c477f328c1
Lynn Meeder <lynn.meeder@stud.uni-goettingen.de>
2026-01-20 18:37:09 +0100

    update the chemcoord interface to handle exceptions

--- ChemCoord_interface.py
+++ ChemCoord_interface.py
@@ -6,30 +6,29 @@ import logging
 # bytecode logging output on the debug level that is not useful
 logging.getLogger('numba').setLevel(logging.WARNING)
 import chemcoord as cc
+from chemcoord.exceptions import UndefinedCoordinateSystem
 
 from helper import pvec_to_crdmat, crdmat_to_pvec
 
+logger = logging.getLogger(__name__)
 
 def total_atom_movement(path_pvecs):
-    result = 0.0
-    for i in range(1, len(path_pvecs)):
-        result += np.linalg.norm(path_pvecs[i] - path_pvecs[i-1])
-    return result
+    """
+    Returns the total atom movement as the sum of the 
+    eucledean norms between image vectors.
+    """
+    path_pvecs = np.array(path_pvecs)
+    diffs = np.diff(path_pvecs, axis=0)
+    movements = np.linalg.norm(diffs, axis=1)
+    return np.sum(movements)
 
 
 def pvec_to_cartesian(labels, pvec):
+    """
+    Convert a pvec to the cartesian object used by chemoord.
+    """
     coords = pvec_to_crdmat(pvec)
-
-    xvals = [coord[0] for coord in coords]
-    yvals = [coord[1] for coord in coords]
-    zvals = [coord[2] for coord in coords]
-
-    framedict = {'atom' : labels,
-                 'x' : xvals,
-                 'y' : yvals,
-                 'z' : zvals}
-
-    frame = pd.DataFrame.from_dict(framedict)
+    frame = pd.DataFrame({'atom': labels, 'x': coords[:,0], 'y': coords[:,1], 'z': coords[:,2]})
     return cc.Cartesian(frame=frame)
 
 
@@ -42,8 +41,14 @@ def pvec_to_zmat(labels, pvec, constr_table=None):
     cc_cart = pvec_to_cartesian(labels, pvec)
 
     if constr_table is None:
-        zmat = cc_cart.get_zmat()
-        constr_table = zmat.loc[:, ['b', 'a', 'd']]
+        try:
+            zmat = cc_cart.get_zmat()
+            constr_table = zmat.loc[:, ['b', 'a', 'd']]
+
+        # this catches for example linear molecules
+        except UndefinedCoordinateSystem as e:
+            logger.warning("ChemCoord could not create Z-matrix: %s", e)
+            return None, None
 
     else:
         zmat = cc_cart.get_zmat(constr_table)
@@ -66,13 +71,16 @@ def zmat_interpolate(start_pvec,
                      labels,
                      interpolations,
                      use_2nd_constr_table=False):
-    """Do an interpolation in z-matrix coordinates using the ChemChord module.
+    """Do an interpolation in z-matrix coordinates using the ChemCoord module.
     Expects:
     - start_pvec: coordinates of start structure
     - end_pvec
     - labels: A list of the atom types
     - interpolations: list of numbers between 0 and 1 for the new interpolations
     - use_2nd_constr_table: True/False to choose the other construction table for a possibly better fit
+
+    Returns the interpolated images in pvec format and the construction table.
+    Returns None if the z-mat construction failed.
     """
     if use_2nd_constr_table:
         zmat2, ctable = pvec_to_zmat(labels, stop_pvec)
@@ -81,6 +89,9 @@ def zmat_interpolate(start_pvec,
         # use 1st construction table instead
         zmat1, ctable = pvec_to_zmat(labels, start_pvec)
         zmat2, ctable = pvec_to_zmat(labels, stop_pvec, constr_table=ctable)
+
+    if any(zmat is None for zmat in [zmat1, zmat2]):
+        return None, None
 
     # perform the actual interpolation
     with cc.TestOperators(False):
